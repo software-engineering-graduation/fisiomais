679e9fed8ba860301b594c96bbee86d166e05b2e
José Victor <jvictormmendesd@gmail.com>
2023-11-29 16:03:56 -0300

    :sparkles: feat: ajustando query para retornar outro tipo de dado cadastros de fisio #92

--- src/backend/src/main/java/com/fisiomais/repository/FisioterapeutaRepository.java
+++ src/backend/src/main/java/com/fisiomais/repository/FisioterapeutaRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 import com.fisiomais.model.Fisioterapeuta;
 import com.fisiomais.model.indicators.NovosFisioterapeutasMetrics;
-import com.fisiomais.model.enums.Mes;
 
 @Repository
 public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, Integer> {
@@ -28,18 +27,21 @@ public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, 
     Fisioterapeuta findByEmail(String email);
 
     @Query("SELECT new com.fisiomais.model.indicators.NovosFisioterapeutasMetrics(" +
-            "m.mes, "+
-            "COALESCE(n.numNovosCadastros, 0) AS numNovosCadastros) " +
+            "m.mes, " +
+            "SUM(COALESCE(n.numNovosCadastros, 0)) OVER (ORDER BY m.mes) AS numNovosCadastros) " +
             "FROM (" +
             "SELECT 1 AS mes " +
             "UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 " +
             "UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 " +
             "UNION SELECT 12) AS m " +
             "LEFT JOIN (" +
-            "   SELECT MONTH(f.create_time) AS mes, COUNT(f) AS numNovosCadastros " +
+            "   SELECT " +
+            "      MONTH(f.create_time) AS mes, " +
+            "      COUNT(f) AS numNovosCadastros " +
             "   FROM Fisioterapeuta f " +
             "   WHERE YEAR(f.create_time) = :anoDesejado " +
             "   GROUP BY MONTH(f.create_time)" +
             ") n ON m.mes = n.mes")
     List<NovosFisioterapeutasMetrics> findNovosCadastrosMensais(@Param("anoDesejado") Integer anoDesejado);
+
 }
