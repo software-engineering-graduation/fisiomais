51b674a9f869c13a661715d169d00b5b936de617
Seu Nome <rubensmarcelo31@gmail.com>
2023-12-02 23:26:41 -0300

    update: front criar tratamento

--- src/frontend/src/pages/Tratamento/NovoTratamento/index.jsx
+++ src/frontend/src/pages/Tratamento/NovoTratamento/index.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import {
-  Form,
-  Input,
-  DatePicker,
-  Button,
-  Select,
-  Checkbox,
-  Table,
-  message
-} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, DatePicker, Button, Select, Checkbox, Table, message, Modal } from 'antd';
 import { BoldOutlined, ItalicOutlined, UnderlineOutlined, OrderedListOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 const NovoTratamento = () => {
   const [form] = Form.useForm();
+  const [pacientes, setPacientes] = useState([]);
+  const [midias, setMidias] = useState([]);
+  const [midiasSelecionadas, setMidiasSelecionadas] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const onFinish = (values) => {
-    // console.log('Plano de tratamento criado:', values);
-    message.success('Plano de tratamento salvo com sucesso!');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resPacientes = await axios.get('http://localhost:8081/api/paciente');
+        const resMidias = await axios.get('http://localhost:8081/api/midia');
+        setPacientes(resPacientes.data);
+        setMidias(resMidias.data);
+      } catch (error) {
+        message.error('Erro ao carregar dados');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const onFinish = async (values) => {
+    try {
+      await axios.post('http://localhost:8081/api/tratamento/novo', values);
+      message.success('Plano de tratamento salvo com sucesso!');
+      form.resetFields();
+    } catch (error) {
+      message.error('Erro ao salvar tratamento');
+    }
+  };
+  const adicionarMidia = (midiaId) => {
+    const midia = midias.find(m => m.id === midiaId);
+    setMidiasSelecionadas(prevMidias => [...prevMidias, midia]);
+    setIsModalVisible(false);
+  };
+
+  const removerMidia = (midiaId) => {
+    setMidiasSelecionadas(midiasSelecionadas.filter(m => m.id !== midiaId));
   };
 
   const onCancel = () => {
@@ -26,7 +51,6 @@ const NovoTratamento = () => {
   return (
     <div>
       <h1>Criar Tratamento</h1>
-
       <Form
         form={form}
         name="tratamento"
@@ -34,10 +58,24 @@ const NovoTratamento = () => {
         labelCol={{ span: 6 }}
         wrapperCol={{ span: 18 }}
       >
-        
+
         <Form.Item
-          label="Título do Texto"
-          name="tituloTexto"
+          label="Paciente"
+          name="pacienteId"
+          rules={[{ required: true, message: 'Por favor, selecione um paciente!' }]}
+        >
+          <Select placeholder="Selecione um paciente">
+            {pacientes.map(paciente => (
+              <Select.Option key={paciente.id} value={paciente.id}>
+                {paciente.nome}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label="Título do Tratamento"
+          name="titulo"
           rules={[
             { required: true, message: 'Por favor, insira o título!', max: 100 }
           ]}
@@ -45,9 +83,9 @@ const NovoTratamento = () => {
           <Input />
         </Form.Item>
         <Form.Item
-          label="Conteúdo do Texto"
-          name="conteudoTexto"
-          rules={[{ required: true, message: 'Por favor, insira o conteúdo!' }]}
+          label="Observações"
+          name="observacoes"
+          rules={[{ required: true, message: 'Por favor, insira a observação!' }]}
         >
           <Input.TextArea rows={4} />
         </Form.Item>
@@ -62,32 +100,43 @@ const NovoTratamento = () => {
 
      
         <Form.Item label="Lista de Mídias">
-          <Table columns={[
-            { title: 'Mídia', dataIndex: 'midia', key: 'midia' },
-            { title: 'Seleção', dataIndex: 'selecao', key: 'selecao', render: () => <Checkbox /> }
-          ]} dataSource={[]} />
+          <Button onClick={() => setIsModalVisible(true)}>Adicionar Mídia</Button>
+          <Table
+            columns={[
+              { title: 'Mídia', dataIndex: 'titulo', key: 'titulo' },
+              {
+                title: 'Ação',
+                key: 'acao',
+                render: (text, record) => (
+                  <Button icon={<DeleteOutlined />} onClick={() => removerMidia(record.id)}>
+                    Remover
+                  </Button>
+                ),
+              },
+            ]}
+            dataSource={midiasSelecionadas}
+            rowKey="id"
+          />
         </Form.Item>
 
     
-        <Form.Item label="Ordem das Atividades">
-          
-        </Form.Item>
         <Form.Item
-          label="Data de Início"
-          name="dataInicio"
-          rules={[{ required: true, message: 'Por favor, selecione a data de início!' }]}
+          label="Feedback"
+          name="feedback"
+          rules={[{ required: true, message: 'Por favor, insira um feedback!' }]}
+        >
+          <Input.TextArea rows={2} />
+        </Form.Item>
+
+ 
+        <Form.Item
+          label="Data de Término"
+          name="end_date"
+          rules={[{ required: true, message: 'Por favor, selecione a data de término!' }]}
         >
           <DatePicker />
         </Form.Item>
-        <Form.Item label="Frequência" name="frequencia">
-          <Select>
-            <Select.Option value="Diário">Diário</Select.Option>
-            <Select.Option value="Semanal">Semanal</Select.Option>
-            <Select.Option value="Quinzenal">Quinzenal</Select.Option>
-            <Select.Option value="Mensal">Mensal</Select.Option>
-          </Select>
-        </Form.Item>
-
+        
         <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
           <Button type="primary" htmlType="submit" style={{ backgroundColor: '#0BD980', borderColor: '#0BD980' }}>
             Salvar Tratamento
@@ -97,6 +146,24 @@ const NovoTratamento = () => {
           </Button>
         </Form.Item>
       </Form>
+      <Modal
+        title="Selecione uma Mídia"
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        footer={null}
+      >
+        <Select
+          style={{ width: '100%' }}
+          placeholder="Selecione uma mídia"
+          onSelect={adicionarMidia}
+        >
+          {midias.map(midia => (
+            <Select.Option key={midia.id} value={midia.id}>
+              {midia.titulo}
+            </Select.Option>
+          ))}
+        </Select>
+      </Modal>
     </div>
   );
 };
